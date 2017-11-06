9c273ceab2f32c16f03e4b5b836a230a8adc6e4a
zhaoyuu <zhaoyuu@yonyou.com>
2017-11-06 14:36:34 +0800

    添加showTime接口

--- build/DatePicker.js
+++ build/DatePicker.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _rcCalendar = require('rc-calendar');
 
 var _rcCalendar2 = _interopRequireDefault(_rcCalendar);
@@ -20,6 +22,14 @@ var _beeFormControl = require('bee-form-control');
 
 var _beeFormControl2 = _interopRequireDefault(_beeFormControl);
 
+var _Panel = require('rc-time-picker/lib/Panel');
+
+var _Panel2 = _interopRequireDefault(_Panel);
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _defaults(obj, defaults) { var keys = Object.getOwnPropertyNames(defaults); for (var i = 0; i < keys.length; i++) { var key = keys[i]; var value = Object.getOwnPropertyDescriptor(defaults, key); if (value && value.configurable && obj[key] === undefined) { Object.defineProperty(obj, key, value); } } return obj; }
@@ -31,6 +41,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : _defaults(subClass, superClass); } /**
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 * Created by chief on 17/4/6.
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 */
+
+var timePickerElement = _react2["default"].createElement(_Panel2["default"], { defaultValue: (0, _moment2["default"])('00:00:00', 'HH:mm:ss') });
 
 var DatePicker = function (_Component) {
     _inherits(DatePicker, _Component);
@@ -67,7 +79,9 @@ var DatePicker = function (_Component) {
 
         var props = this.props;
 
-        var calendar = _react2["default"].createElement(_rcCalendar2["default"], props);
+        var calendar = _react2["default"].createElement(_rcCalendar2["default"], _extends({
+            timePicker: props.showTime ? timePickerElement : null
+        }, props));
 
         return _react2["default"].createElement(
             'div',
